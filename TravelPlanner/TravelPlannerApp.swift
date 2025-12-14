1cc0f3969c551f1c442371326a9d17702e15b81c
Juraj <juraj.vepy@gmail.com>
2025-12-14 15:15:35 +0100

    Initial Commit

---
+++ TravelPlanner/TravelPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelPlannerApp.swift
+//  TravelPlanner
+//
+//  Created by Juraj Vépy on 14/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
